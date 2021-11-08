f424f7bf30b51b76c60c5777c76b47e9be62e76f
Jesse Kottonen <jesse.kottonen@gmail.com>
2021-11-08 14:58:28 +0200

    Fixed alignment.

--- assets/colors.js
+++ assets/colors.js
@@ -2,6 +2,7 @@ export default {
     primaryBackground: "#DBF5F0",
     SecondaryBackground: "#37BEB0",
     textColor: "#22223B",
+    textColorSecondary: "yellow",
     liningColor: "#9A8C98",
     formWhite: "#FFF",
 }
